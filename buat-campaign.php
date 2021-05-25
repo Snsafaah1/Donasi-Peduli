42e5d5fb335645c251cfa804fa57a5d75b456029
gege1310 <gigehp13@gmail.com>
2021-05-25 23:06:44 +0700

    jajal

--- buat-campaign.php
+++ buat-campaign.php
@@ -6,7 +6,7 @@
 	<head>
 		<meta charset="utf-8">
 <<<<<<< HEAD
-		<title>Buat Campaignnn say.</title>
+		<title>Buat Campaignnn no say</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
 
 		<!-- <link rel="shortcut icon" href=""> -->
