79e64e09d0c92dd68e231101f075515081f02dc5
alyachoirum <alyachoirum@gmail.com>
2021-05-27 21:10:04 +0700

    manambah fitur upload foto

--- aksibuatcampaign.php
+++ aksibuatcampaign.php
@@ -11,41 +11,40 @@
 	$diri = $_POST['diri'];
 	$kategori = $_POST['kategori'];
 	$judul = $_POST['judul'];
-	$waktu = "2021-05-25 15:18:33";
+	$waktu = $_POST['waktu'];
 	$penerima = $_POST['penerima'];
 	$kondisi = $_POST['kondisi'];
 	$rencana = $_POST['rencana'];
 	$ajakan = $_POST['ajakan'];
 	$nomor = $_POST['nomor'];
+	
+	// ambil data foto campaign
+	$namaFile = $_FILES['foto']['name'];
+	$namaSementara = $_FILES['foto']['tmp_name'];
 
-	$ekstensi_diperbolehkan	= array('png','jpg');
-	$foto = $_FILES['foto']['name'];
-	$x = explode('.', $foto);
-	$ekstensi = strtolower(end($x));
-	$file_tmp = $_FILES['foto']['tmp_name'];
+	// tentukan lokasi file akan dipindahkan
+	$dirUpload = "foto/";
 
-	if (!empty($foto)){
-	    if (in_array($ekstensi, $ekstensi_diperbolehkan) === true){
+	// pindahkan file
+	$terupload = move_uploaded_file($namaSementara, $dirUpload.$namaFile);
 
-	        //Mengupload ga
-	        move_uploaded_file($file_tmp, 'foto/'.$foto);
-	        
-	    }
-	}else {
-	    $foto="bank_default.png";
+	if ($terupload) {
+		$user = mysqli_query($conn,"SELECT * FROM user where email = '$email' "); //ambil data dari database
+		$user = mysqli_fetch_assoc($user); //merubah konversi array
+		$id_user = $user['id_user']; // menyimpan variable id user
+
+		$query= "INSERT INTO 
+		campaign (id_user, id_kategori, batas_waktu, judul, pekerjaan, medsos, alasan_penggalangan, tempat_tinggal, kondisi_terkini, instansi, profil_diri, penerima_manfaat, ajakan_singkat, foto, nomor_hp, status) 
+		VALUES ('$id_user','$kategori','$waktu','$judul', '$pekerjaan','$medson','$rencana','$tempat','$kondisi','$instansi','$diri','$penerima','$ajakan','$namaFile','$nomor','proses')";
+		$insert= mysqli_query($conn,$query);
+		if($insert == TRUE){
+			header('location: index.php');
+		}else{
+			header('location: buat-campaign.php?alert=gagal buat campaign');
+		}
+	} else {
+	    echo "Upload Gagal!";
 	}
 
-	$user = mysqli_query($conn,"SELECT * FROM user where email = '$email' "); //ambil data dari database
-	$user = mysqli_fetch_assoc($user); //merubah konversi array
-	$id_user = $user['id_user']; // menyimpan variable id user
-
-	$query= "INSERT INTO 
-	campaign (id_user, id_kategori, batas_waktu, judul, pekerjaan, medsos, alasan_penggalangan, tempat_tinggal, kondisi_terkini, instansi, profil_diri, penerima_manfaat, ajakan_singkat, foto, nomor_hp, status) 
-	VALUES ('$id_user','$kategori','$waktu','$judul', '$pekerjaan','$medson','$rencana','$tempat','$kondisi','$instansi','$diri','$penerima','$ajakan','foto','$nomor','proses')";
-	$insert= mysqli_query($conn,$query);
-	if($insert == TRUE){
-		header('location: index.php');
-	}else{
-		header('location: buat-campaign.php?alert=gagal buat campaign');
-	}
+	
 ?>
